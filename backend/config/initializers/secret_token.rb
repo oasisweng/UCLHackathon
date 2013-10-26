10fe6c5ac5480cacc8e47e1f85905a44776714d1
Kaihua Li <kaihua.tinkerbox.com.sg>
2013-10-26 23:20:59 +0100

    backend

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backend::Application.config.secret_key_base = '8ab288a2cff9b950e9c64a00ed94f9e5f7dd81c0ac2d87334cc53793da35842866b24688bdf720736cd4e48af2489818c7a558233b0797b92a0bf8577319322c'
